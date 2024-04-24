7f6d26efdcfc7c7cef7a7f2e24a54b24b2e5dc1f
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-04-24 13:00:38 -0600

    Finished Array question 3 (problem 26)

--- Top_Interview_150/array_3_remove_duplicates_p26.py
+++ Top_Interview_150/array_3_remove_duplicates_p26.py
@@ -7,6 +7,9 @@ class Solution(object):
         i = 0
         j = 1
         n_unique = len(nums)
+        idxs_unique = []
+        if nums:
+            idxs_unique.append(nums[0])
         while j < len(nums):
             if nums[i] == nums[j]:
                 nums[j] = "_"
@@ -15,6 +18,10 @@ class Solution(object):
             else:
                 i = j
                 j += 1
+                idxs_unique.append(i)
+        for i in range(n_unique):
+            if idxs_unique[i] != i:
+                nums[i], nums[idxs_unique[i]] = nums[idxs_unique[i]], nums[i]
         return n_unique
 
 ################################################################################
