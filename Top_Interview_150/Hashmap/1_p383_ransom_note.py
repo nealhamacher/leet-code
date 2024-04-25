9ab148b66a1b6c1f8667e75fa72b49ead7666c44
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-04-25 10:34:32 -0600

    Completed Hashmap 1, Problem 383

---
+++ Top_Interview_150/Hashmap/1_p383_ransom_note.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        letters = {}
+        for char in magazine:
+            if char not in letters.keys():
+                letters[char] = 1
+            else:
+                letters[char] += 1
+        for char in ransomNote:
+            if char not in letters.keys():
+                return False
+            elif letters[char] == 0:
+                return False
+            else:
+                letters[char] -= 1
+        return True
+
+
+################################################################################
+
+if __name__ == "__main__":
+    sol = Solution()
+    ransomNote = "a" 
+    magazine = "b"
+    print(sol.canConstruct(ransomNote, magazine))
+    ransomNote = "aa" 
+    magazine = "ab"
+    print(sol.canConstruct(ransomNote, magazine))
+    ransomNote = "aa" 
+    magazine = "aab"
+    print(sol.canConstruct(ransomNote, magazine))
+
