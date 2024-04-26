7c3ec2217bc29dcd0cddc9fe31a35f7ec8a2f5d0
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-04-26 16:22:58 -0600

    Completed intervals 1, problem 228

---
+++ Top_Interview_150/Intervals/1_p228_summary_ranges.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        if len(nums) == 0:
+            return []
+        else:
+            results = []
+            idx1 = 0
+            i = 1
+            while i < len(nums):
+                if nums[i] != nums[idx1] + i - idx1:
+                    idx2 = i-1
+                    s = str(nums[idx1])
+                    if idx2 != idx1:
+                        s += ("->" + str(nums[idx2]))
+                    results.append(s)
+                    idx1 = i
+                else:
+                    i += 1
+            s = str(nums[idx1])
+            if idx1 != len(nums)-1:
+                s += ("->" + str(nums[len(nums)-1]))
+            results.append(s)
+    
+        return results
+    
+
+################################################################################
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [0,1,2,4,5,7]
+    print(sol.summaryRanges(nums))
+    nums = [0,2,3,4,6,8,9]
+    print(sol.summaryRanges(nums))
+
+        
