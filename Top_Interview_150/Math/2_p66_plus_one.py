f709b1326aa40d7ea3f683dd56dbec122c4398e4
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-04-26 18:32:09 -0600

    Modified math 2 problem 66 to not alter passed list

--- Top_Interview_150/Math/2_p66_plus_one.py
+++ Top_Interview_150/Math/2_p66_plus_one.py
@@ -20,7 +20,20 @@ class Solution(object):
         if not incremented:
             ret.insert(0, 1)
         return ret
-    
+
+    def plusOneInPlace(self, digits):
+        i = len(digits) - 1
+        while i >= 0:
+            if digits[i] != 9:
+                digits[i] += 1
+                break
+            else:
+                digits[i] = 0
+                i -= 1
+        if i == -1:
+            digits.insert(0, 1)
+        return digits
+
 
 ################################################################################
 
@@ -28,15 +41,21 @@ if __name__ == "__main__":
     sol = Solution()
     digits = [1,2,3]
     print(sol.plusOne(digits))
+    print(sol.plusOneInPlace(digits))
     digits = [4,3,2,1]
     print(sol.plusOne(digits))
+    print(sol.plusOneInPlace(digits))
     digits = [9]
     print(sol.plusOne(digits))
+    print(sol.plusOneInPlace(digits))
     digits = [9,1,9]
     print(sol.plusOne(digits))
+    print(sol.plusOneInPlace(digits))
     digits = [9,9,9]
     print(sol.plusOne(digits))
+    print(sol.plusOneInPlace(digits))
     digits = [8,9,9]
     print(sol.plusOne(digits))
+    print(sol.plusOneInPlace(digits))
     
     
