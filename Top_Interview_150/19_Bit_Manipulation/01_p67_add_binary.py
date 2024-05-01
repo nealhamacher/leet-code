2f24c11f67cd3231d7de90ab2b0698a7ccd139e1
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-05-01 17:56:13 -0600

    Completed Bit Manipulation 1, Problem 67 with int conversions - probably not shortest  / most efficient way to do this

---
+++ Top_Interview_150/19_Bit_Manipulation/01_p67_add_binary.py
@@ -0,0 +1,55 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        carry = 0
+        sum = ""
+        if len(a) >= len(b):
+            long = a
+            short = b
+        else:
+            long = b
+            short = a
+        for i in range(len(short)):
+            d1 = int(a[len(a)-i-1])
+            d2 = int(b[len(b)-i-1])
+            added = d1 + d2 + carry
+            if added == 0:
+                sum = "0" + sum
+                carry = 0
+            if added == 1:
+                sum = "1" + sum
+                carry = 0
+            if added == 2:
+                sum = "0" + sum
+                carry = 1
+            if added == 3:
+                sum = "1" + sum
+                carry = 1
+        for i in range(len(long)-len(short)):
+            added = int(long[len(long)-len(short)-i-1]) + carry
+            if added == 0:
+                sum = "0" + sum
+                carry = 0
+            if added == 1:
+                sum = "1" + sum
+                carry = 0
+            if added == 2:
+                sum = "0" + sum
+                carry = 1
+        if carry == 1:
+            sum = "1" + sum
+        return sum
+    
+
+################################################################################
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.addBinary("11", "1"))
+    print(sol.addBinary("1010", "1011"))
+
+
