22d6e2d21014e5f9fb55938606b90e8888d07cc7
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-05-04 20:09:17 -0600

    Completed Divide & Conquer Q1, Problem 108 - Sorted Array to Binary Search Tree

--- Top_Interview_150/16_Divide_&_Conquer/01_p108_sorted_arry_to_bst.py
+++ Top_Interview_150/16_Divide_&_Conquer/01_p108_sorted_arry_to_bst.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution(object):
     def sortedArrayToBST(self, nums):
         """
@@ -11,19 +11,27 @@ class Solution(object):
         :rtype: TreeNode
         """
         if len(nums) == 0:
-            return ["null"]
-        elif len(nums) == 1:
-            return [nums[0]]
+            return None
+        if len(nums) == 1:
+            return TreeNode(nums[0])
         
         else:
             mid_idx = (len(nums)-1)//2
-            bst = [nums[mid_idx]]
-            bst += self.sortedArrayToBST(nums[0:mid_idx])
-            bst += self.sortedArrayToBST(nums[mid_idx+1:len(nums)])
-            return bst
-
+            return TreeNode(
+                nums[mid_idx],
+                self.sortedArrayToBST(nums[0:mid_idx]),
+                self.sortedArrayToBST(nums[mid_idx+1:len(nums)])
+            )
+            
 
 ################################################################################
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.sortedArrayToBST([-10,-3,0,5,9]))
+    root = sol.sortedArrayToBST([-10,-3,0,5,9])
+    print(root.val)
+    print(root.left.val)
+    print(root.right.val)
+    print(root.left.left)
+    print(root.left.right.val)
+    print(root.right.left)
+    print(root.right.right.val)
