52c350aa342e00137a82f9163e36a5c7bb088909
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-05-07 14:52:46 -0600

    Completed trie question 1, problem 208 - implement trie

---
+++ Top_Interview_150/14_Trie/01_p208_implement_trie.py
@@ -0,0 +1,63 @@
+class Node(object):
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+        current = self.root
+        for letter in word:
+            if letter not in current.children.keys():
+                current.children[letter] = Node()
+            current = current.children[letter]
+        current.isEnd = True
+        
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        current = self.root
+        for letter in word:
+            if letter not in current.children.keys():
+                return False
+            current = current.children[letter]
+        if current.isEnd:
+            return True
+        else:
+            return False
+        
+
+    def startsWith(self, prefix):
+        """
+        :type prefix: str
+        :rtype: bool
+        """
+        current = self.root
+        for letter in prefix:
+            if letter not in current.children.keys():
+                return False
+            current = current.children[letter]
+        return True
+
+
+################################################################################
+
+if __name__ == "__main__":
+    trie = Trie()
+    trie.insert("apple")
+    print(trie.search("apple"))
+    print(trie.search("app"))
+    print(trie.startsWith("app"))
+    trie.insert("app")
+    print(trie.startsWith("app"))
+    print(trie.search("appleb"))
