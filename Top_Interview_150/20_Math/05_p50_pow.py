3ce98ca8c28ed7507a180b0819e975401abe131c
Neal <nealhamacher@gmail.com>
2024-05-07 18:06:25 -0600

    Completed iterative solution for  math question 5, problem 50 - power

--- Top_Interview_150/20_Math/05_p50_pow.py
+++ Top_Interview_150/20_Math/05_p50_pow.py
@@ -7,18 +7,21 @@ class Solution(object):
         """
         if n == 0:
             return 1
-        if n == 1:
-            return x
         if n < 0:
             return 1/self.myPow(x, -n)
-        
-        return self.myPow(x*x, n-1)
+        pow = x
+        while n > 1:
+            pow *= x
+            n -= 1
+        return pow
     
 
 ################################################################################
 
 if __name__ == "__main__":
     sol = Solution()
+    print(sol.myPow(2, 10))
     print(sol.myPow(2.1, 3))
+    print(sol.myPow(2, -2))
         
         
