e0374f1e1cd7fc4fd93275ee6ae72ec202ac8aaf
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-05-09 08:55:08 -0600

    Second optimized solution for hashmap question 5, problem 1 - two sum

--- Top_Interview_150/05_Hashmap/06_p1_two_sum.py
+++ Top_Interview_150/05_Hashmap/06_p1_two_sum.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    # Simple solution - O(n**2)
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
@@ -10,7 +11,17 @@ class Solution(object):
                 if nums[i] + nums[j] == target:
                     return [i, j]
         return None
-    
+
+    # Optimized solution using hashmap
+    def twoSum2(self, nums, target):
+        difference_map = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in difference_map.keys():
+                return [difference_map[diff], i]
+            else:
+                difference_map[nums[i]] = i
+        return None
 
 ################################################################################
 
@@ -19,3 +30,7 @@ if __name__ == "__main__":
     print(sol.twoSum([2,7,11,15], 9))
     print(sol.twoSum([3,2,4], 6))
     print(sol.twoSum([3,3], 6))
+    
+    print(sol.twoSum2([2,7,11,15], 9))
+    print(sol.twoSum2([3,2,4], 6))
+    print(sol.twoSum2([3,3], 6))
