89dfac3d09e342ff7de2854f9e548367678ea702
Neal <nealhamacher@gmail.com>
2024-05-10 13:35:58 -0600

    Completed bit manipulation question 2, problem 190 - reverse bits

---
+++ Top_Interview_150/19_Bit_Manipulation/02_p190_reverse_bits.py
@@ -0,0 +1,13 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        return int(n[::-1], 2)
+    
+
+################################################################################
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.reverseBits('00000010100101000001111010011100'))
+    print(sol.reverseBits('11111111111111111111111111111101'))
