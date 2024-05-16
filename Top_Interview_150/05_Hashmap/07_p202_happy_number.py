e7f0fa0cf911fd9366bb1c646b5dd4c5c95bddc3
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-05-15 22:05:41 -0600

    Alternate solution to hashmap q7, p202 - happy number, using Floyd's tortoise and hare algorithm

--- Top_Interview_150/05_Hashmap/07_p202_happy_number.py
+++ Top_Interview_150/05_Hashmap/07_p202_happy_number.py
@@ -1,10 +1,10 @@
 class Solution(object):
+    # Using list
     def isHappy(self, n):
         """
         :type n: int
         :rtype: bool
         """
-        # Using List
         n_str = str(n)
         seen = []
         result = 0
@@ -18,13 +18,39 @@ class Solution(object):
                     digit = int(digit_char)
                     result += digit * digit
                 n_str = str(result)
-                print(result)
         return True
     
+    # Using Floyd's tortoise and hare
+    def isHappyFloyd(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        def calculateNext(number):
+            result = 0
+            while number > 0:
+                last_digit = number % 10
+                result += last_digit * last_digit
+                number = number // 10
+            return result
+        
+        tortoise = calculateNext(n)
+        print(tortoise)
+        hare = calculateNext(calculateNext(n))
+        while hare != 1:
+            if tortoise == hare:
+                return False
+            else:
+                tortoise = calculateNext(tortoise)
+                print(tortoise)
+                hare = calculateNext(calculateNext(hare))
+        return True
 
 ################################################################################
 
 if __name__ == "__main__":
     sol = Solution()
     print(sol.isHappy(19))
+    print(sol.isHappyFloyd(19))
     print(sol.isHappy(2))
+    print(sol.isHappyFloyd(2))
