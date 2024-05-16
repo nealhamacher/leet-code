ace5de52bf003ef1a70b1bd9616ef4d1a82a9c94
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-05-16 13:21:09 -0600

    Improved two pointers q1, p125 - valid palindrome

--- Top_Interview_150/02_Two_Pointers/01_p125_valid_palindrome.py
+++ Top_Interview_150/02_Two_Pointers/01_p125_valid_palindrome.py
@@ -6,15 +6,38 @@ class Solution(object):
         """
         stripped = ""
         for char in s:
-            if char.isalpha():
+            if char.isspace():
+                continue
+            elif char.isalpha():
                 stripped += char.lower()
+            else:
+                stripped += char
 
         for i in range((len(stripped)+1)//2):
             if stripped[i] != stripped[len(stripped)-1-i]:
                 return False
         return True
 
-
+    def isPalindrome2(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            while not s[i].isalnum():
+                i += 1
+                if i > j:
+                    return True
+            while not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
+    
 ################################################################################
 
 if __name__ == "__main__":
