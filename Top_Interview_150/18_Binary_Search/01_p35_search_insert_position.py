c09423a8cfa8e211a8a6870639ae6ba7ff9e4efb
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-05-20 12:50:13 -0600

    Optimized recursive solution for binary search q1, p35 - search insert position
    Increased space efficiency at cost of time efficiency, by tracking if start or end was moved before doing array-element comparision in recursive search function

--- Top_Interview_150/18_Binary_Search/01_p35_search_insert_position.py
+++ Top_Interview_150/18_Binary_Search/01_p35_search_insert_position.py
@@ -1,29 +1,34 @@
 class Solution(object):
     # Recursive Solution
-    # Runtime: 33 ms, beats 25.10%. Memory: 12.26 MB, beats 88.27% 
+    # After optimizing - Runtime: 27 ms, beats 74.83%. Memory: 12.48 MB, beats 23.51% 
+    # Original - Runtime: 33 ms, beats 25.10%. Memory: 12.26 MB, beats 88.27% 
     def searchInsert(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        def searchHelper(nums, start, end, target):
-            if target > nums[end]:
+        def searchHelper(nums, start, end, target, movedStart):
+            if not movedStart and target > nums[end]:
                 return end + 1
-            if target < nums[start]:
+            if movedStart and target < nums[start]:
                 return start
 
             mid = (end + start) // 2
-            if nums[mid] == target:3
+            if nums[mid] == target:
                 return mid
             elif nums[mid] > target:
-                return searchHelper(nums, start, mid - 1, target)
+                return searchHelper(nums, start, mid - 1, target, False)
             else:
-                return searchHelper(nums, mid+1, end, target)
+                return searchHelper(nums, mid+1, end, target, True)
         
         start = 0
         end = len(nums) - 1
-        return searchHelper(nums, start, end, target)
+        if nums[end] < target:
+            return end + 1
+        if nums[start] > target:
+            return start
+        return searchHelper(nums, start, end, target, False)
     
 
     # Iterative Solution
