6800c9a79ccc52659daab18ea0b91b3e680f7f81
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-05-20 12:13:29 -0600

    Alternate answer for  sliding window q2, p3 - longest substring without repeats
    Uses start and end index instead of copy of substring, a bit slower but less memory used than previous answer.

--- Top_Interview_150/03_Sliding_Window/02_p3_longest_substring_no_repeats.py
+++ Top_Interview_150/03_Sliding_Window/02_p3_longest_substring_no_repeats.py
@@ -1,4 +1,6 @@
 class Solution(object):
+    # Runtime: 182 ms, beats 21.90%. Memory: 12.59 MB, beats 32.76%
+    # Uses copy of substring
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
@@ -24,6 +26,28 @@ class Solution(object):
             return length
         return longest
     
+    def lengthOfLongestSubstring2(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+        else:
+            start = 0
+            end = 0
+            longest = 1
+        for i in range(1, len(s)):
+            for j in range(end, start-1, -1):
+                if end - start + 1 > longest:
+                    longest = end - start + 1
+                if s[i] == s[j]:
+                    start = j+1
+                    end = i
+                    break
+            else:
+                end += 1
+        return longest
 
 ################################################################################
 
@@ -32,5 +56,9 @@ if __name__ == "__main__":
     print(sol.lengthOfLongestSubstring("abcabcbb"))
     print(sol.lengthOfLongestSubstring("bbbbb"))
     print(sol.lengthOfLongestSubstring("pwwkew"))
+    print("\n")
+    print(sol.lengthOfLongestSubstring2("abcabcbb"))
+    print(sol.lengthOfLongestSubstring2("bbbbb"))
+    print(sol.lengthOfLongestSubstring2("pwwkew"))
     
     
