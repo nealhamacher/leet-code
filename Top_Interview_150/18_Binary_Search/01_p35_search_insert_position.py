fa6f778ecf749e7384c219bcd3b3fecb1179a37a
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-05-20 12:45:17 -0600

    Completed iterative solution for binary search q1, p35 - search insert position

--- Top_Interview_150/18_Binary_Search/01_p35_search_insert_position.py
+++ Top_Interview_150/18_Binary_Search/01_p35_search_insert_position.py
@@ -1,11 +1,12 @@
 class Solution(object):
+    # Recursive Solution
+    # Runtime: 33 ms, beats 25.10%. Memory: 12.26 MB, beats 88.27% 
     def searchInsert(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        # Runtime: 33 ms, beats 25.10%. Memory: 12.26 MB, beats 88.27% 
         def searchHelper(nums, start, end, target):
             if target > nums[end]:
                 return end + 1
@@ -13,7 +14,7 @@ class Solution(object):
                 return start
 
             mid = (end + start) // 2
-            if nums[mid] == target:
+            if nums[mid] == target:3
                 return mid
             elif nums[mid] > target:
                 return searchHelper(nums, start, mid - 1, target)
@@ -25,6 +26,28 @@ class Solution(object):
         return searchHelper(nums, start, end, target)
     
 
+    # Iterative Solution
+    # Runtime: 35 ms, beats 15.41%. Memory: 12.36 MB, beats 59.51% 
+    def searchInsert2(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start = 0
+        end = len(nums) - 1 
+        mid = (end + start) // 2
+        while nums[mid] != target:
+            if target < nums[start]:
+                return start
+            elif target > nums[end]:
+                return end + 1
+            elif target > nums[mid]:
+                start = mid + 1
+            else:
+                end = mid - 1
+            mid = (end + start) // 2
+        return mid
         
 
 ################################################################################
@@ -37,4 +60,11 @@ if __name__ == "__main__":
     print(sol.searchInsert([1,3,5,6], 0))
     print(sol.searchInsert([1,3], 0))
     print(sol.searchInsert([1,3,5,6], 7))
+    print("\n")
+    print(sol.searchInsert2([1,3,5,6], 5))
+    print(sol.searchInsert2([1,3,5,6], 2))
+    print(sol.searchInsert2([1,3,5,6], 7))
+    print(sol.searchInsert2([1,3,5,6], 0))
+    print(sol.searchInsert2([1,3], 0))
+    print(sol.searchInsert2([1,3,5,6], 7))
 
