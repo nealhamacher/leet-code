3501d53311cb61ec0731b0f0c2213c465373680c
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-06-04 15:23:50 -0600

    Improved time efficiency of Array/String question 16, problem 13, roman to integer
    Improvements made by changing to elif statements

--- Top_Interview_150/01_Array_String/16_p13_roman_to_integer.py
+++ Top_Interview_150/01_Array_String/16_p13_roman_to_integer.py
@@ -1,6 +1,41 @@
 class Solution(object):
-    # Runtime - 34 ms, beats 36.96%, Memory - 11.63MB, beats 52.60%
+    # Runtime - 16 ms, beats 97.47%, Memory - 11.51MB, beats 83.49%
     def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i = 0
+        sum = 0
+        while i < len(s):
+            if s[i] == 'M':
+                sum += 1000
+            elif s[i] == 'D':
+                sum += 500
+            elif s[i] == 'C': 
+                if (i < len(s) - 1) and (s[i+1] == 'M' or s[i+1] == 'D'):
+                    sum -= 100
+                else:
+                    sum += 100
+            elif s[i] == 'L':
+                sum += 50
+            elif s[i] == 'X':
+                if (i < len(s) - 1) and (s[i+1] == 'C' or s[i+1] == 'L'):
+                    sum -= 10
+                else:
+                    sum += 10
+            elif s[i] == 'V':
+                sum += 5
+            elif s[i] == 'I':
+                if (i < len(s) - 1) and (s[i+1] == 'X' or s[i+1] == 'V'):
+                    sum -= 1
+                else:
+                    sum += 1
+            i += 1
+        return sum
+
+    # Runtime - 34 ms, beats 36.96%, Memory - 11.63MB, beats 52.60%
+    def romanToIntOLD(self, s):
         """
         :type s: str
         :rtype: int
@@ -39,6 +74,10 @@ class Solution(object):
 
 if __name__ == "__main__":
     sol = Solution()
+    print(sol.romanToIntOLD("III"))
+    print(sol.romanToIntOLD("LVIII"))
+    print(sol.romanToIntOLD("MCMXCIV"))
+
     print(sol.romanToInt("III"))
     print(sol.romanToInt("LVIII"))
     print(sol.romanToInt("MCMXCIV"))
